ab198f1a0db8a8d7a249c16eaf491e1f09134080
Idan Gigi <gigiidan@gmail.com>
2019-07-04 20:57:17 +0300

    adding axios

--- src/components/Navigation/Toolbar/Toolbar.js
+++ src/components/Navigation/Toolbar/Toolbar.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import classes from './Toolbar.module.css';
 import Logo from '../../Logo/Logo';
-import NavgationItems from  '../NavigationItems/NavigationItems';
+import NavigationItems from  '../NavigationItems/NavigationItems';
 const toolbar = (props) => {
     return (
        <header className={classes.Toolbar}>
            <div>MENU</div>
            <Logo />
            <nav>
-               <NavgationItems></NavgationItems>
+               <NavigationItems></NavigationItems>
            </nav>
        </header>
     );
